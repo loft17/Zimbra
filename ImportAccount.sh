cd1c29d423de05d381acfdf5169c8c6a652b722e
loft17 <loft17@gmail.com>
2020-04-16 17:43:18 +0200

    update

--- ImportAccount.sh
+++ ImportAccount.sh
@@ -4,7 +4,7 @@
 #                                                                                                            Info
 #----------------------------------------------------------------------------------------------------------------
 # name: ImportAllUserZimbra
-# version: 1.0
+# version: 1.5
 # autor: joseRomera <web@joseromera.net>
 # web: http://www.joseromera.net
 #----------------------------------------------------------------------------------------------------------------
@@ -32,8 +32,6 @@ COS_NM2="professional2"
 COS_ID1="087bf9be-2c81-4f5f-bb8a-170b21714ca2"
 COS_ID2="376daa10-d0fc-4aaa-87b1-7cb12ffe9c66"
 
-
-cp -f  ListAllAccount.csv2 ListAllAccount.csv
 #----------------------------------------------------------------------------------------------------------------
 #                                                                                                       Funciones
 #----------------------------------------------------------------------------------------------------------------
@@ -42,8 +40,11 @@ sed -i 's/'$COS_NM2'/'$COS_ID2'/'  $FOLDER/$LISTADO
 sed -i 's/'$COS_NM1'/'$COS_ID1'/'  $FOLDER/$LISTADO
 
 cat $LISTADO | while read LINEA; do
- #echo "  LINEA = $LINEA"
- 
+ if [ "$DEBUG" == "1" ]
+  then
+   echo "  LINEA = $LINEA"
+ fi
+
  CAMPO1=$(echo $LINEA | cut -d ";" -f 1)
  CAMPO2=$(echo $LINEA | cut -d ";" -f 2)
  CAMPO3=$(echo $LINEA | cut -d ";" -f 3)
@@ -52,15 +53,18 @@ cat $LISTADO | while read LINEA; do
  CAMPO6=$(echo $LINEA | cut -d ";" -f 6)
  CAMPO7=$(echo $LINEA | cut -d ";" -f 7) 
  CAMPO8=$(echo $LINEA | cut -d ";" -f 8)
-		
- #echo "  - Referencia = $CAMPO1"
- #echo "  - Direccion de correo = $CAMPO2"
- #echo "  - Nombre Mostrado = $CAMPO3"
- #echo "  - Nombre = $CAMPO4"
- #echo "  - Apellido = $CAMPO5"
- #echo "  - UID = $CAMPO6"
- #echo "  - Password = $CAMPO7"
- #echo "  - COS = $CAMPO8"
+
+ if [ "$DEBUG" == "1" ]
+  then
+   echo "  - Referencia = $CAMPO1"
+   echo "  - Direccion de correo = $CAMPO2"
+   echo "  - Nombre Mostrado = $CAMPO3"
+   echo "  - Nombre = $CAMPO4"
+   echo "  - Apellido = $CAMPO5"
+   echo "  - UID = $CAMPO6"
+   echo "  - Password = $CAMPO7"
+   echo "  - COS = $CAMPO8"
+ fi
 
  echo "zmprov ca $CAMPO2 $CAMPO7 displayName '"$CAMPO3"' givenName '"$CAMPO4"' sn '"$CAMPO5"' zimbraCOSid $CAMPO8"
  zmprov ca $CAMPO2 $CAMPO7 displayName "$CAMPO3" givenName "$CAMPO4" sn "$CAMPO5" zimbraCOSid $CAMPO8
