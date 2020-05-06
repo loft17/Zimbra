e682799d30dd2ef28f90aa850bf62679261f2f8a
loft17 <loft17@gmail.com>
2020-05-06 15:27:50 +0200

    Update 1.5

--- ExportAllAccountAranSalut.sh
+++ ExportAllAccountAranSalut.sh
@@ -3,8 +3,8 @@
 #----------------------------------------------------------------------------------------------------------------
 #                                                                                                            Info
 #----------------------------------------------------------------------------------------------------------------
-# name: ExportAllUserZimbra
-# version: 1.0
+# name: ExportAllUserZimbraAranSalut
+# version: 1.5
 # autor: joseRomera <web@joseromera.net>
 # web: http://www.joseromera.net
 #----------------------------------------------------------------------------------------------------------------
@@ -28,24 +28,30 @@ COS=$(echo "displayName zimbraAccountStatus zimbraLastLogonTimestamp zimbraCreat
 # Limpiamos listado
 cat /dev/null > $FOLDER/$LISTADO
 
-;;087bf9be-2c81-4f5f-bb8a-170b21714ca2;20200417063252.545Z;;378,45 MB
-
 # Agregamos los campos
-echo "email;displayName;Status;COSId;Created;description;Size" >> $FOLDER/$LISTADO
+echo "email;displayName;Status;COSId;Created;description;Size;Alias" >> $FOLDER/$LISTADO
 
 # Conseguimos todas las cuentas
 CUENTAS=`zmprov -l gaa | egrep -v 'admin|wiki|galsync|spam|ham|virus'`;
 
 # loop for each CUENTA
 for EMAIL in ${CUENTAS}; do
+  RESULTADO_ALIAS=""
   # Sacamos la informacion de la cuenta
   RESULTADO=`zmprov -l ga $EMAIL $COS | grep -v "#" | awk '!/^$/' | awk -F": " '{print $2";"}' | tr -d '\n' | head -c-1`
   DESCRIPTION=`zmprov -l ga $EMAIL description | grep -v "#" | awk '!/^$/' | awk -F": " '{print $2";"}' | tr -d '\n' | head -c-1`
   # Sacamos el tamaño del buzon
   MB_SIZE=`zmmailbox -z -m ${EMAIL} gms | tr . ,`
-  
-  # Creamos el fichero
-  echo "$EMAIL;$RESULTADO;$DESCRIPTION;$MB_SIZE" >> $FOLDER/$LISTADO
+  # Averiguamos si tiene alias la cuenta
+  RESULTADO_ALIAS=`zmprov -l ga $EMAIL zimbraMailAlias | grep -v "#" | grep .  | awk -F: '{print $1}'`
+  # Agregamos alias si es necesario
+  if [[ $RESULTADO_ALIAS == "zimbraMailAlias" ]]
+  then
+    MEMALIAS=`zmprov -l ga $EMAIL zimbraMailAlias | grep -v "#" | awk -F "zimbraMailAlias: " '{print $2}' | grep . | awk 'ORS=";"' | head -c-1`
+        echo "$EMAIL;$RESULTADO;$DESCRIPTION;$MB_SIZE;$MEMALIAS" >> $FOLDER/$LISTADO
+  else
+    echo "$EMAIL;$RESULTADO;$DESCRIPTION;$MB_SIZE" >> $FOLDER/$LISTADO
+  fi
 done
 
 echo done
